1d1051216b0453eb86d5eb6600b6e4e2424ed2c1
Etienne Pogoda <pogoda.etienne@outlook.fr>
2023-04-20 16:14:46 +0200

    Code OK

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
-import bootstrap from "bootstrap";
+var btn2 = document.getElementById("btn2");
+let xp = document.getElementById("experiences");
+let formations = document.getElementById("formations");
+let ci = document.getElementById("ci");
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if (e.style.display == "block") {
+    e.style.display = "none";
+  } else {
+    xp.style.display = "none";
+    formations.style.display = "none";
+    ci.style.display = "none";
+    e.style.display = "block";
+  }
+  console.log("clicked");
+
+  return;
+}
