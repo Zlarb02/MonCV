d5021052f052a20b9daaf9dafa099d292c5d0132
Etienne Pogoda <pogoda.etienne@outlook.fr>
2023-04-25 00:45:14 +0200

    feat (change style) : work fine but zelda style is ugly
    change photo, bg color and font when user click on music buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-var btn2 = document.getElementById("btn2");
 let xp = document.getElementById("experiences");
 let formations = document.getElementById("formations");
 let ci = document.getElementById("ci");
@@ -16,3 +15,33 @@ function toggle_visibility(id) {
 
   return;
 }
+
+let btnL = document.getElementById("btn-for-left-sidebar");
+let btnR = document.getElementById("btn-for-right-sidebar");
+
+let col1 = document.getElementById("col-left");
+let col2 = document.getElementById("col-right");
+
+let pic = document.getElementById("profilePic");
+
+let changeFont = document.getElementById("change-font");
+
+btnL.addEventListener("click", () => {
+  col1.style.backgroundColor = "antiqueWhite";
+  col2.style.backgroundColor = "cornsilk";
+  col1.style.color = "black";
+  col2.style.color = "black";
+  pic.src = "img/Etienne-Pogoda-profile-picture.jpg";
+  pic.style.borderRadius = "100%";
+  changeFont.style.fontFamily = "'Roboto', sans-serif";
+});
+
+btnR.addEventListener("click", () => {
+  col1.style.backgroundColor = "#B5EAAD";
+  col2.style.backgroundColor = "#AB99FA";
+  col1.style.color = "#3f385f";
+  col2.style.color = "#122d0d";
+  pic.src = "img/Etienne-Pogoda-profile-picture-alternative.jpg";
+  pic.style.borderRadius = "15px";
+  changeFont.style.fontFamily = "'Poppins', sans-serif";
+});
